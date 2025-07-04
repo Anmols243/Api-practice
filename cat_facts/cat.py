f8e075870b1533cea57958c450cc4097e8385099
Anmol Singh <168528590+Anmols243@users.noreply.github.com>
2025-07-04 14:44:07 +0530

    Updated code

--- cat_facts/cat.py
+++ cat_facts/cat.py
@@ -2,14 +2,21 @@ import requests
 import json
 
 api_url = "https://catfact.ninja/fact"
+facts = set()
+i = 0
 
 try:
-    response = requests.get(api_url)
-    response.raise_for_status()
-    fact = response.json()['fact']
+    while len(facts) < 5:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        fact = response.json()['fact']
+        facts.add(fact)
+    
+    for i, fact in enumerate(facts, start=1):
+        print(f"{i}. {fact}\n")
     
 except Exception as e:
     print(e)
+    
 
-else:
-    print(fact)
+
